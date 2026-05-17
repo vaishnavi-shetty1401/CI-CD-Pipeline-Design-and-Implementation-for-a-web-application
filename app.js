fc268d67638aa0d0e4d49aaffc49c240eb18f71f
Vaishnavi Shetty <vaishnavi123@gmail.com>
2026-05-17 20:24:23 +0530

    Updated app for Railway deployment

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
 app.get('/', (req, res) => {
   res.send('CI/CD Pipeline Success!');
 });
 
-app.listen(3000, () => console.log('Server running on port 3000'));
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
